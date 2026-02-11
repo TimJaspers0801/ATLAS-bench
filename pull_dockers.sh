28034e0174c7641178c87671227498ac1bcdb119
TimJaspers <55140275+TimJaspers@users.noreply.github.com>
2026-02-11 11:51:56 +0100

    debugging

--- pull_dockers.sh
+++ pull_dockers.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --nodes=1                               # Specify the amount of Nodes
 #SBATCH --ntasks=1                              # Specify the number of tasks
-#SBATCH --cpus-per-task=16                      # Specify the number of CPUs/task
+#SBATCH --cpus-per-task=12                      # Specify the number of CPUs/task
 #SBATCH --gpus=1                                # Specify the number of GPUs to use
-#SBATCH --partition=gpu_h100                    # Specify the node partition
+#SBATCH --partition=gpu_a100                    # Specify the node partition
 #SBATCH --time=12:00:00                         # Specify the maximum time the job can run
 
 
