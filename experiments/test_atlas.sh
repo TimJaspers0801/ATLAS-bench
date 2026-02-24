64cad1df64d6ffdb7230fd5b39cb7576960e7df3
TimJaspers <55140275+TimJaspers@users.noreply.github.com>
2026-02-24 14:52:12 +0100

    eomt vitl imagenet

--- experiments/test_atlas.sh
+++ experiments/test_atlas.sh
@@ -77,6 +77,8 @@ MODELS=(
 
     # # EOMT models
     # "eomt_vitl_dinov3|best_model.pth|eomt_dinov3_vitl_surgenet_256|0|32"
+    "eomt_vitl_dinov3|best_model.pth|eomt_dinov3_vitl_256|0|32"
+
     
     # # # # SurgeNet Baselines
     # "surgenet-pvtv2-b2|best_model.pth|pvtv2_atlas|0|32"
