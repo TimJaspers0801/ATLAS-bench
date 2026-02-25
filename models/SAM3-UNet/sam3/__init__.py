76bab842a4b56d7881453ddc8821529548cce1b8
TimJaspers <55140275+TimJaspers@users.noreply.github.com>
2026-02-25 17:22:59 +0100

    Bug SAM3

--- models/SAM3-UNet/sam3/__init__.py
+++ models/SAM3-UNet/sam3/__init__.py
@@ -1,6 +1,8 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates. All Rights Reserved
 
-from .model_builder import build_sam3_image_model
+def build_sam3_image_model(*args, **kwargs):
+	from .model_builder import build_sam3_image_model as _build
+	return _build(*args, **kwargs)
 
 __version__ = "0.1.0"
 
